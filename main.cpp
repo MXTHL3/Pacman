f6d8dc4a0fa92a25b6da01ab18fc74512c67b743
MXTHL3 <116813446+MXTHL3@users.noreply.github.com>
2023-01-05 17:15:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <vector>
 #include <math.h>
+#include <stdlib.h>
 #include "MinGL2/include/mingl/mingl.h"
 #include "MinGL2/include/mingl/shape/rectangle.h"
 
@@ -12,26 +14,62 @@ double winHeight=600;
 double colNum=51;
 double rowNum=31;
 double colX1,colX2,rowY1,rowY2;
+int r;
 
+vector<vector <int>> mapMemory (rowNum, vector<int>(colNum,0));
+
+bool initMapVar=true
+void initMap(){
+    for (unsigned int row=0; row<rowNum; ++row){
+        for (unsigned int col=0; col<colNum; ++col){
+            if (col%2==0 and row%2==0){
+               mapMemory[col][row]=1;
+               cout << mapMemory[col][row];
+            }
+        }
+    }
+    initMapVar=false;
+}
 
 void show(MinGL &window){
     //window << Rectangle(Vec2D(30, 30), Vec2D(30, 30), KWhite);
     for (unsigned int row=0; row<rowNum; ++row){
         for (unsigned int col=0; col<colNum; ++col){
-            if (col%2==0 and row%2==0){
-                colX1=round(winWidth/colNum*col);
-                colX2=round(winHeight/rowNum*row);
-                rowY1=round(winWidth/colNum*(col+1));
-                rowY2=round(winHeight/rowNum*(row+1));
-                window << Rectangle(Vec2D(colX1, colX2),
-                                    Vec2D(rowY1, rowY2),
-                                    KBlue);
+            colX1=round(winWidth/colNum*col);
+            colX2=round(winHeight/rowNum*row);
+            rowY1=round(winWidth/colNum*(col+1));
+            rowY2=round(winHeight/rowNum*(row+1));
+            //if (mapMemory[col][row]==1){
+            //    window << Rectangle(Vec2D(colX1, colX2),
+            //                        Vec2D(rowY1, rowY2),
+            //                        KBlue);
+            //}
+
+            if (col<colNum/2+1){
+                r=rand()%2;
+                if (r==1){
+                    if (row%2==1){
+                        window << Rectangle(Vec2D(colX1, colX2),
+                                            Vec2D(rowY1, rowY2),
+                                            KBlue);
+                    }
+                    if (row%2==0){
+                        window << Rectangle(Vec2D(colX1, colX2),
+                                            Vec2D(rowY1, rowY2),
+                                            KBlue);
+                    }
+                }
             }
+
+
         }
     }
 }
 
 int main(){
+    if (initMapVar==true){
+       initMap();
+    }
     MinGL window("Pacman", Vec2D(winWidth, winHeight), Vec2D(0, 0), KBlack);
     window.initGlut();
     window.initGraphic();
@@ -44,3 +82,4 @@ int main(){
     }
     return 0;
 }
+
